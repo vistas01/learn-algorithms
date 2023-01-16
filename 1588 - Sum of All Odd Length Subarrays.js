c2c0fc7818bfe164a59f4b274075a6538d8973d9
vistas01 <vistas01sills@icloud.com>
2023-01-16 23:15:04 +0900

    created 1588 - Sum of All Odd Length Subarrays

---
+++ 1588 - Sum of All Odd Length Subarrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    let arr = coordinates.map(el => {
+        return {x: el[0], y:el[1]}
+    }) 
+    const slope = (arr[1].y - arr[0].y) / (arr[1].x - arr[0].x);
+    function cal_slope(p1, p2){
+        return (p2.y - p1.y) / (p2.x - p1.x)
+    }
+    for(let i = 1; i < arr.length; i++){
+        if(cal_slope(arr[0], arr[i]) !== slope) return false;
+    }
+    return true;
+}
+checkStraightLine([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]);
