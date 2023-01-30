43570f634506ca5fa507eaf859b42159869e8391
vistas01 <vistas01sills@icloud.com>
2023-01-30 23:51:32 +0900

    created 876

---
+++ 876.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let len = findLength(head, 0);
+  return findIndex(head, Math.floor((len - 1) / 2) + 1, 0);
+  function findLength(node, idx) {
+    if (node.next === null) return idx;
+    return findLength(node.next, idx + 1);
+  }
+  function findIndex(node, idx, count) {
+    console.log(node, idx, count);
+    if (count === idx) return node;
+    return findIndex(node.next, idx, count + 1);
+  }
+};
