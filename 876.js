260ee0c11222c8e58f08808ec31458a7824f580e
vistas01 <vistas01sills@icloud.com>
2023-01-31 23:49:18 +0900

    find better solution

--- 876.js
+++ 876.js
@@ -22,3 +22,27 @@ var middleNode = function (head) {
     return findIndex(node.next, idx, count + 1);
   }
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  var middle = head;
+  var end = head;
+
+  while (end) {
+    if (!end.next) break;
+    end = end.next.next;
+    middle = middle.next;
+  }
+
+  return middle;
+};
