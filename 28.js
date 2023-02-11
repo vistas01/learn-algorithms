769d3278688b8e1299530126b9ab89fb1d1915de
vistas01 <vistas01sills@icloud.com>
2023-02-11 23:35:17 +0900

    fix: solve using indexof

--- 28.js
+++ 28.js
@@ -4,15 +4,5 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  let res = -1;
-  for (let i = 0; i < haystack.length; i++) {
-    if (haystack[i] === needle[0]) {
-      let res = i;
-      for (let j = 0; j < needle.length; j++) {
-        if (haystack[i + j] !== needle[j]) res = -1;
-      }
-      if (res !== -1) return res;
-    }
-  }
-  return res;
+  return haystack.indexOf(needle);
 };
