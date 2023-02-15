b999b61af66fd100fbfd61cac2f5116ec324d7f5
vistas01 <vistas01sills@icloud.com>
2023-02-15 23:17:03 +0900

    fix: reduce time complexity

--- 150.js
+++ 150.js
@@ -3,16 +3,14 @@
  * @return {number}
  */
 var evalRPN = function (tokens) {
-  var evalRPN = function (tokens) {
-    while (tokens.length > 1) {
-      for (let i = 0; i < tokens.length; i++) {
-        if (isNaN(Number(tokens[i]))) {
-          let cal = tokens[i - 2] + " " + tokens[i] + " " + tokens[i - 1];
-          tokens.splice(i - 2, 3, String(Math.trunc(eval(cal))));
-          break;
-        }
-      }
+  let i = 0;
+  while (tokens.length > 1) {
+    if (isNaN(Number(tokens[i]))) {
+      let cal = tokens[i - 2] + " " + tokens[i] + " " + tokens[i - 1];
+      tokens.splice(i - 2, 3, String(Math.trunc(eval(cal))));
+      i -= 2;
     }
-    return Number(tokens[0]);
-  };
+    i++;
+  }
+  return Number(tokens[0]);
 };
