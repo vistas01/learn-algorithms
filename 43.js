986dcc56315045f0e30398e4cad0d7cbdcad6736
vistas01 <vistas01sills@icloud.com>
2023-02-21 19:12:49 +0900

    solve: 91ms

--- 43.js
+++ 43.js
@@ -4,15 +4,44 @@
  * @return {string}
  */
 var multiply = function (num1, num2) {
-  let n1 = num1.split("");
-  let n2 = num2.split("");
-  let res = n1.reduce((acc, cur, i) => {
-    //console.log(acc, cur);
-    return (acc += n2.reduce((acc2, cur2, i2) => {
-      console.log(cur, cur2);
-      return (acc2 +=
-        cur * cur2 * Math.pow(10, n1.length - i + (n2.length - i2) - 2));
-    }, 0));
-  }, 0);
-  return res.toString();
+  let n1 = new Map();
+  let n2 = new Map();
+  num1
+    .split("")
+    .reverse()
+    .map((num, i) => {
+      n1.set(i, Number(num));
+    });
+  num2
+    .split("")
+    .reverse()
+    .map((num, i) => {
+      n2.set(i, Number(num));
+    });
+  let prod = new Map();
+  n1.forEach((val1, key1) => {
+    n2.forEach((val2, key2) => {
+      if (prod.has(key1 + key2)) {
+        prod.set(key1 + key2, prod.get(key1 + key2) + val1 * val2);
+      } else {
+        prod.set(key1 + key2, val1 * val2);
+      }
+    });
+  });
+  prod.forEach((val, key) => {
+    if (val > 9) {
+      if (prod.has(key + 1)) {
+        prod.set(key + 1, prod.get(key + 1) + Math.trunc(val / 10));
+      } else {
+        prod.set(key + 1, Math.trunc(val / 10));
+      }
+      prod.set(key, val % 10);
+    }
+  });
+  let res = [...prod]
+    .map(([key, val]) => val.toString())
+    .reverse()
+    .join("")
+    .replace(/\b0+/g, "");
+  return res.length !== 0 ? res : "0";
 };
