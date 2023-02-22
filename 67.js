3c3d47b6edf537764ddf538a85bfd2d4ef0b6cbb
vistas01 <vistas01sills@icloud.com>
2023-02-22 21:59:48 +0900

    fix: reduce if statement

--- 67.js
+++ 67.js
@@ -5,22 +5,33 @@
  */
 var addBinary = function (a, b) {
   if (a.length > b.length) {
-    var longer = a.split("").reverse();
-    var shorter = b.split("").reverse();
+    var longer = a
+      .split("")
+      .reverse()
+      .map((el) => Number(el));
+    var shorter = b
+      .split("")
+      .reverse()
+      .map((el) => Number(el));
   } else {
-    var longer = b.split("").reverse();
-    var shorter = a.split("").reverse();
+    var longer = b
+      .split("")
+      .reverse()
+      .map((el) => Number(el));
+    var shorter = a
+      .split("")
+      .reverse()
+      .map((el) => Number(el));
   }
   for (let i = 0; i < longer.length; i++) {
-    longer[i] = (Number(longer[i]) + Number(shorter[i] ?? "0")).toString();
-    if (Number(longer[i]) === 2) {
-      longer[i] = Number(0).toString();
-      longer[i + 1] = (Number(longer[i + 1] ?? "0") + 1).toString();
-    }
-    if (Number(longer[i]) === 3) {
-      longer[i] = Number(1).toString();
-      longer[i + 1] = (Number(longer[i + 1] ?? "0") + 1).toString();
+    longer[i] += shorter[i] ?? 0;
+    if (longer[i] > 1) {
+      longer[i] = longer[i] % 2;
+      longer[i + 1] = (longer[i + 1] ?? 0) + 1;
     }
   }
-  return longer.reverse().join("");
+  return longer
+    .map((el) => el.toString())
+    .reverse()
+    .join("");
 };
