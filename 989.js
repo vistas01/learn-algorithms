ef6930d4e0a0012ac7aaa85ce463d7f9922819fd
vistas01 <vistas01sills@icloud.com>
2023-02-23 23:35:27 +0900

    fix: solve using unshift

--- 989.js
+++ 989.js
@@ -4,19 +4,12 @@
  * @return {number[]}
  */
 var addToArrayForm = function (num, k) {
-  num.reverse();
-  k.toString()
-    .split("")
-    .reverse()
-    .map((el) => Number(el))
-    .forEach((val, i) => {
-      num[i] = (num[i] ?? 0) + val;
-    });
-  num.forEach((val, i) => {
-    if (val > 9) {
-      num[i + 1] = (num[i + 1] ?? 0) + Math.trunc(val / 10);
-      num[i] = val % 10;
-    }
-  });
-  return num.reverse();
+  let res = [],
+    i = num.length;
+  while (i-- > 0 || k) {
+    k += num[i] ?? 0;
+    res.unshift(k % 10);
+    k = Math.trunc(k / 10);
+  }
+  return res;
 };
